2e2889c9e1d366dfec05db4b184779ace5f9181b
rouaret.gipsy@orange.fr <Gipsy1403>
2026-02-13 10:30:52 +0100

    Corrige accessibilite selon WCAG page accueil et page media

--- components/layout/PhotographerMedia.jsx
+++ components/layout/PhotographerMedia.jsx
@@ -42,89 +42,91 @@ export default function PhotographerMedia({photographer, imagesPhotographer,tota
 	return (
 		<>
 			<HeaderPhotographer />
-			<article className={styles.photographers}>
+			<main>
+				<article className={styles.photographers}>
+					<section>
+						<h1 className={styles.name}>{photographer.name}</h1>
+						<p className={styles.city}>{photographer.city}, {photographer.country}</p>
+						<p className={styles.tagline}>{photographer.tagline}</p>
+					</section>
+					<button className={styles.contact_me} onClick={() => setModalOpen(true)}>
+						Contactez-moi
+					</button>
+					<ContactModal
+						open={modalOpen}
+						close={() => setModalOpen(false)}
+						title={`Contactez ${photographer.name}` }
+						photographer={photographer}>
+					</ContactModal>
+					<Image
+						className={styles.portrait}
+						src={`/assets/${photographer.portrait}`}
+						alt={photographer.name}
+						width={200}
+						height={200}
+					/>
+				</article>
 				<section>
-					<h1 className={styles.name}>{photographer.name}</h1>
-					<h3 className={styles.city}>{photographer.city}, {photographer.country}</h3>
-					<h4 className={styles.tagline}>{photographer.tagline}</h4>
-				</section>
-				<button className={styles.contact_me} onClick={() => setModalOpen(true)}>
-					Contactez-moi
-				</button>
-				<ContactModal 
-					open={modalOpen} 
-					close={() => setModalOpen(false)} 
-					title={`Contactez ${photographer.name}` }
-					photographer={photographer}>
-				</ContactModal>
-				<Image
-					className={styles.portrait}
-					src={`/assets/${photographer.portrait}`}
-					alt={photographer.name}
-					width={200}
-					height={200}
-				/>
-			</article>
-			<section>
-				<div className={styles.order_by} >
-					<label className={styles.label_sort} htmlFor="order_by">Trier par</label>
-					<select
-						className={styles.select}
-						id="order_by"
-						value={sortBy}
-						onChange={(e) => setSortBy(e.target.value)}
-					>
-						<option className={styles.options}value="likes" >Popularité</option>
-						<option className={styles.options} value="title">Titre</option>
-						<option className={styles.options} value="date">Date</option>
-					</select>
-				</div>
-				<div className={styles.container_media}>
-					{sortedMedias.map((pictures,index)=>(
-					<div className={styles.card} key={pictures.id} role="button" tabIndex={0}
-					onClick={()=>{
-						setMediaIndex(index);
-						setMediaModalOpen(true);
-					}}
-					onKeyDown={(e) => {
-						if (e.key === "Enter" || e.key === " ") {
-							e.preventDefault();
+					<div className={styles.order_by} >
+						<label className={styles.label_sort} htmlFor="order_by">Trier par</label>
+						<select
+							className={styles.select}
+							id="order_by"
+							value={sortBy}
+							onChange={(e) => setSortBy(e.target.value)}
+						>
+							<option className={styles.options}value="likes" >Popularité</option>
+							<option className={styles.options} value="title">Titre</option>
+							<option className={styles.options} value="date">Date</option>
+						</select>
+					</div>
+					<div className={styles.container_media}>
+						{sortedMedias.map((pictures,index)=>(
+						<div className={styles.card} key={pictures.id} role="button" tabIndex={0}
+						onClick={()=>{
 							setMediaIndex(index);
 							setMediaModalOpen(true);
-						}
-					}}>
-						{pictures.image ? (
-						// Si c'est une image
-						<Image
-							className={styles.picture}
-							src={`/assets/${pictures.image}`}
-							alt={pictures.title}
-							width={350}
-							height={300}
-							priority
-						/>
-						) : pictures.video ? (
-						// Si c'est une vidéo
-						<div className={styles.video} >
-							<FontAwesomeIcon className={styles.icon} icon={faVideo} />
+						}}
+						onKeyDown={(e) => {
+							if (e.key === "Enter" || e.key === " ") {
+								e.preventDefault();
+								setMediaIndex(index);
+								setMediaModalOpen(true);
+							}
+						}}>
+							{pictures.image ? (
+							// Si c'est une image
+							<Image
+								className={styles.picture}
+								src={`/assets/${pictures.image}`}
+								alt={pictures.title}
+								width={350}
+								height={300}
+								priority
+							/>
+							) : pictures.video ? (
+							// Si c'est une vidéo
+							<div className={styles.video} >
+								<FontAwesomeIcon className={styles.icon} icon={faVideo} />
+							</div>
+							) : null}
+							<div className={styles.description}>
+								<h3 className={styles.title}>{pictures.title}</h3>
+								<Likes mediaId={pictures.id} initialLikes={pictures.likes} onLike={incrementAllLikes} />
+							</div>
 						</div>
-						) : null}
-						<div className={styles.description}>
-							<h3 className={styles.title}>{pictures.title}</h3>
-							<Likes mediaId={pictures.id} initialLikes={pictures.likes} onLike={incrementAllLikes} />
-						</div>
+						))}
+						<MediaModal
+							open={mediaModalOpen}
+							photographer={photographer}
+							imagesPhotographer={sortedMedias}
+							startIndex={mediaIndex}
+							close={() => setMediaModalOpen(false)}>
+						</MediaModal>
+						<TotalLikes totalLikesPhotographer={allLikes} photographer={photographer}/>
 					</div>
-					))}
-					<MediaModal
-						open={mediaModalOpen}
-						photographer={photographer}
-						imagesPhotographer={sortedMedias}
-						startIndex={mediaIndex}
-						close={() => setMediaModalOpen(false)}>
-					</MediaModal>
-					<TotalLikes totalLikesPhotographer={allLikes} photographer={photographer}/>
-				</div>
-			</section>
+				</section>
+			</main>
 		</>
 	)
 }
